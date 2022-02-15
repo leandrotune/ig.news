5d1378d305c293dbb237a32bada89f6dfb057a68
Leandro Antunes <lele.tune@gmail.com>
2022-02-14 21:39:19 -0300

    Salvando usuario no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb'
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -13,4 +17,22 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+
+      } catch {
+        return false
+      }
+    },
+  }
 });
